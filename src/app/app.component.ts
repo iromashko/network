045930c56cc25309ced10448864c447a2fe453ee
iromashko <iromashko@me.com>
2020-04-23 04:34:07 +0300

    event term

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'network';
+  onTerm(term: string) {
+    console.log(`I am the app and here is the term ${term}`);
+  }
 }
--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-search-bar',
-  templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+  selector: "app-search-bar",
+  templateUrl: "./search-bar.component.html",
+  styleUrls: ["./search-bar.component.scss"],
 })
 export class SearchBarComponent implements OnInit {
+  @Output() submitted = new EventEmitter<string>();
+  term = "";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onFormSubmit(event: any) {
+    event.preventDefault();
+    this.submitted.emit(this.term);
   }
-
 }
