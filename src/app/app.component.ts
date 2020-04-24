86f4b7a0c73031b486e289767f720c3ecc621789
iromashko <iromashko@me.com>
2020-04-24 05:07:45 +0300

    add table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { WikipediaService } from "./wikipedia.service";
 
 @Component({
   selector: "app-root",
@@ -6,7 +7,13 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
+  pages = [];
+
+  constructor(private wikipediaService: WikipediaService) {}
+
   onTerm(term: string) {
-    console.log(`I am the app and here is the term ${term}`);
+    this.wikipediaService.search(term).subscribe((response: any) => {
+      this.pages = response.query.search;
+    });
   }
 }
--- src/app/page-list/page-list.component.ts
+++ src/app/page-list/page-list.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-page-list',
-  templateUrl: './page-list.component.html',
-  styleUrls: ['./page-list.component.scss']
+  selector: "app-page-list",
+  templateUrl: "./page-list.component.html",
+  styleUrls: ["./page-list.component.scss"],
 })
 export class PageListComponent implements OnInit {
+  @Input()
+  pages = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
